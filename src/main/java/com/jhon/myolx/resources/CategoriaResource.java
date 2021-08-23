446910015a352dd79286e251953209f47ac69955
Jonathan <jonathanxavier556@gmail.com>
2021-08-23 17:35:35 -0300

    Testando classe categoria

--- src/main/java/com/jhon/myolx/resources/CategoriaResource.java
+++ src/main/java/com/jhon/myolx/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.jhon.myolx.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jhon.myolx.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -10,7 +15,15 @@ public class CategoriaResource {
 
 	
 	@GetMapping
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
